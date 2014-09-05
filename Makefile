7e8d720a5658ea3258d7c5095385caae8cf12839
Alif Rachmawadi <subosito@gmail.com>
2014-09-05 18:29:30 +0700

    fixed test env

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
 fmt:
 	go fmt ./...
 test:
-	rice embed-go
 	godep go test -check.v
-	rm *.rice-box.go
 cov:
 	rice embed-go; godep go test -cover -coverprofile=coverage.out
 cover:
--- pkgo_test.go
+++ pkgo_test.go
@@ -3,6 +3,7 @@ package pkgo
 import (
 	"github.com/subosito/gotenv"
 	check "gopkg.in/check.v1"
+	"log"
 	"os"
 	"testing"
 )
@@ -12,7 +13,10 @@ func Test(t *testing.T) { check.TestingT(t) }
 type BaseSuite struct{}
 
 func (s *BaseSuite) SetUpSuite(c *check.C) {
-	gotenv.MustLoad(".env.test")
+	err := gotenv.Load(".env.test")
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func (s *BaseSuite) TearDownSuite(c *check.C) {
