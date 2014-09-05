05239609c48532751ba9155949d97e84b413198a
Alif Rachmawadi <subosito@gmail.com>
2014-09-05 18:47:31 +0700

    load test env when available only

--- pkgo_test.go
+++ pkgo_test.go
@@ -3,22 +3,37 @@ package pkgo
 import (
 	"github.com/subosito/gotenv"
 	check "gopkg.in/check.v1"
-	"log"
 	"os"
 	"testing"
 )
 
 func Test(t *testing.T) { check.TestingT(t) }
 
-type BaseSuite struct{}
+type BaseSuite struct {
+	loadenv bool
+}
 
 func (s *BaseSuite) SetUpSuite(c *check.C) {
-	err := gotenv.Load(".env.test")
-	if err != nil {
-		log.Println(err)
+	tf := ".env.test"
+
+	if s.isFileExist(tf) {
+		err := gotenv.Load(tf)
+		if err == nil {
+			s.loadenv = true
+		}
 	}
 }
 
 func (s *BaseSuite) TearDownSuite(c *check.C) {
-	os.Clearenv()
+	if s.loadenv {
+		os.Clearenv()
+	}
+}
+
+func (s *BaseSuite) isFileExist(fn string) bool {
+	if _, err := os.Stat(fn); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
 }
